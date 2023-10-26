4acb2be4944ad49e54a92dbed82069d5d3b09980
Aarav 238 <aarav8090shukla@gmail.com>
2023-10-27 00:13:33 +0530

    feat: added alert

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -7,6 +7,7 @@ const countryCodes = require("country-codes-list");
 
 const ReservationForm = () => {
   const navigate = useNavigate();
+  const [alert, setAlert] = useState(false);
   const myCountryCodesObject = countryCodes.customList(
     "countryCode",
     "+{countryCallingCode} {countryNameEn} "
@@ -47,9 +48,11 @@ const ReservationForm = () => {
     e.preventDefault();
     console.log(formData);
     localStorage.setItem("reserve", JSON.stringify(formData));
-    navigate("/success")
-    
-    // Add logic to handle the form submission (e.g., send data to a server).
+    setAlert(true);
+    setTimeout(() => {
+      navigate("/success");
+    }, 1000);
+
   };
 
   return (
@@ -196,11 +199,56 @@ const ReservationForm = () => {
               <textarea
                 id="comment"
                 name="comment"
+                value={formData.comment}
                 rows="4"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                 placeholder="Any Other Requirements...."
               />
             </div>
+            {alert && (
+              <div
+                id="alert-border-3"
+                class="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-400"
+                role="alert"
+              >
+                <svg
+                  class="flex-shrink-0 w-4 h-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <div class="ml-3 text-sm font-medium">
+                  Successfully Reserved. Thank you!!
+                </div>
+                <button
+                  onClick={() => setAlert(false)}
+                  type="button"
+                  class="ml-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8"
+                  data-dismiss-target="#alert-border-3"
+                  aria-label="Close"
+                >
+                  <span class="sr-only">Dismiss</span>
+                  <svg
+                    class="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
             <div>
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full w-full focus:outline-none focus:ring focus:ring-blue-300 cursor-pointer"
@@ -211,7 +259,7 @@ const ReservationForm = () => {
             </div>
           </form>
         </div>
-        <div 
+        <div
           className="w-full bg-cover bg-center hidden md:flex"
           style={{ backgroundImage: `url(${cardImg})` }}
         ></div>
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Success = () => {
-
+    const navigate = useNavigate()
     const reserveData = JSON.parse(localStorage.getItem("reserve"))
-    console.log(reserveData)
+    const handleClick = () => {
+        localStorage.clear();
+        navigate("/")
+    }
     
   return (
-    <>
+    
+    <div className='flex flex-col items-center'>
     <div>Thank You for taking our service , <span className='font-bold'>{reserveData.name}!!</span></div>
-    <button>R</button>
-    </>
+    <button onClick={handleClick} className='p-2 bg-blue-600 m-2 rounded-lg text-white hover:bg-blue-900'>Reserve again</button>
+    </div>
+    
+    
  
     
   )
