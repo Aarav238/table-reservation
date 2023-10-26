ce44616d57e54206d23c836671abeb9811cbb7f7
Aarav 238 <aarav8090shukla@gmail.com>
2023-10-27 00:48:52 +0530

    bugs fixed

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -200,6 +200,7 @@ const ReservationForm = () => {
                 id="comment"
                 name="comment"
                 value={formData.comment}
+                onChange={handleInputChange}
                 rows="4"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                 placeholder="Any Other Requirements...."
